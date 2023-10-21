425f66122c9b88f56465549fd90f091a69f3e457
Nergy-TCGeneric <5158870+Nergy-TCGeneric@users.noreply.github.com>
2023-10-21 10:59:09 +0900

    fix: Extend commandset a bit

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import serial
 import struct
 
-print("Enter the port: ")
-port = input()
+YDLIDAR_START_SIGN = bytearray([0xA5, 0x5A])
+SCANNING = 0x81
+HEALTH_STATUS = 0x06
+DEVICE_INQUIRY = 0x04
+
+port = input("Enter the port: ")
+cmd_query = input("Enter the command: ")
+
 ser = serial.Serial(port, 512000)
-
-print("Enter the command: ")
-cmd_query = input()
-
 command = bytearray() 
+
 if cmd_query == "start":
     command = bytearray([0xA5, 0x60])
 elif cmd_query == "stop":
@@ -17,6 +20,17 @@ elif cmd_query == "dinfo":
     command = bytearray([0xA5, 0x90])
 elif cmd_query == "dhealth":
     command = bytearray([0xA5, 0x92])
+elif cmd_query == "scanfreq":
+    command = bytearray([0xA5, 0x0D])
+elif cmd_query == "rangfreq":
+    command = bytearray([0xA5, 0xD1])
+elif cmd_query == "pprotect":
+    command = bytearray([0xA5, 0xD9])
+elif cmd_query == "reset":
+    command = bytearray([0xA5, 0x40])
+else:
+    print("Invalid command given. exiting")
+    exit()
 
 ser.write(command)
 
@@ -24,12 +38,15 @@ start_sign = ser.read(2)
 response = ser.read(4)
 typecode = ser.read(1)
 
-if start_sign == bytearray([0xA5, 0x5A]):
+if start_sign == YDLIDAR_START_SIGN:
     print("Retrieved response successfully.")
-    print("Response length : ", struct.unpack('<4B', response)[0])
-    print("Response type : ", struct.unpack('<B', typecode)[0])
+    res_length = struct.unpack('<4B', response)[0]
+    res_type = struct.unpack('<B', typecode)[0]
+
+    print("Response length : ", res_length)
+    print("Response type : ", res_type)
     
-    payload = ser.read(20)
+    payload = ser.read(res_length)
     print("Payload: ", payload)
 else:
     print("Failed to retrieve the response.")
