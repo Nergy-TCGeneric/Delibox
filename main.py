c7fdd7aa25eed2088dc05ee7fcd9322ead50ad39
Nergy-TCGeneric <5158870+Nergy-TCGeneric@users.noreply.github.com>
2023-10-21 11:24:34 +0900

    fix: Add read and write timeout for serial

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ SOFT_RESTART = 0x40
 port = input("Enter the port: ")
 cmd_query = input("Enter the command: ")
 
-ser = serial.Serial(port, 512000)
+ser = serial.Serial(port, 512000, timeout=2, write_timeout=2)
 command = bytearray([0xA5, 0x00]) 
 
 if cmd_query == "start":
