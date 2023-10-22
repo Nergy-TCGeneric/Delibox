fc7893fe26dcf4dc987d428b221d12e40bebe4b7
Nergy-TCGeneric <5158870+Nergy-TCGeneric@users.noreply.github.com>
2023-10-22 16:53:36 +0900

    fix: Continuously fetch cloud data from LiDAR

--- main.py
+++ main.py
@@ -54,11 +54,13 @@ ser.write(command)
 
 start_sign = ser.read(2)
 response = ser.read(4)
-typecode = ser.read(1)
+typecode = ser.read()
 
 res_mode = response[3] >> 6
 
 if start_sign == YDLIDAR_START_SIGN:
+    ser.read() # This is necessary to strip one surplus zero-padded byte
+
     print("Retrieved response successfully.")
     res_length = struct.unpack('<4B', response)[0]
     res_type = struct.unpack('<B', typecode)[0]
@@ -68,30 +70,35 @@ if start_sign == YDLIDAR_START_SIGN:
     print("Response mode : ", res_mode)
 
     if res_mode == CONTINUOUS:
-        header = ser.read(2)
-        status = ser.read()
-        sample_quantity = ser.read()
-        start_angle = ser.read(2)
-        end_angle = ser.read(2)
-        checkcode = ser.read(2)
-
-        status = int.from_bytes(status, 'little')
-        quantity = int.from_bytes(sample_quantity, 'little')
-        fsa = int.from_bytes(start_angle, 'little')
-        lsa = int.from_bytes(end_angle, 'little')
-
-        print("Packet header sanity check : ", header == bytearray([0x55, 0xAA]))
-        print("Freq : ", ((status & 0b11111110) >> 1) / 10)
-        print("Current packet type : ", status & 0b1)
-        print("Sample quantity : ", quantity)
-
-        print("Starting angle :", (fsa >> 1) / 64)
-        print("End angle : ", (lsa >> 1) / 64)
-        print("Checkcode : ", checkcode)
-
         while True:
-            sample_data = ser.read(3)
-            print(sample_data)
+            header = ser.read(2)
+            status = ser.read()
+            sample_quantity = ser.read()
+            start_angle = ser.read(2)
+            end_angle = ser.read(2)
+            checkcode = ser.read(2)
+
+            print(header.hex(), status.hex(), sample_quantity.hex())
+            print(start_angle.hex(), end_angle.hex(), checkcode.hex())
+
+            status = int.from_bytes(status, 'little')
+            quantity = int.from_bytes(sample_quantity, 'little')
+            fsa = int.from_bytes(start_angle, 'little')
+            lsa = int.from_bytes(end_angle, 'little')
+
+            print("Packet header sanity check : ", header == bytearray([0xAA, 0x55]))
+            print("Freq : ", ((status & 0b11111110) >> 1) / 10)
+            print("Current packet type : ", status & 0b1)
+            print("Sample quantity : ", quantity)
+
+            print("Starting angle :", (fsa >> 1) / 64)
+            print("End angle : ", (lsa >> 1) / 64)
+            print("Checkcode : ", checkcode)
+
+            for i in range(0, quantity):
+                sample_data = ser.read(3)
+                print(sample_data.hex())
+
     elif res_mode == SINGLE:
         payload = ser.read(res_length)
         print("Payload: ", payload)
