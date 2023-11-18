26e4e5c1f33fdb37dbd46056a8cfe502aca71161
Nergy-TCGeneric <5158870+Nergy-TCGeneric@users.noreply.github.com>
2023-11-18 10:30:13 +0900

    fix: Create G2 LiDAR module

---
+++ lidar/g2.py
@@ -0,0 +1,138 @@
+import serial
+from typing import List
+from typing import Tuple 
+from dataclasses import dataclass
+
+import math
+
+# Byte sequence constant.
+SCAN_HEADER = (bytes([0xAA]), bytes([0x55]))
+
+# Command byte constants.
+START_SCAN = bytearray([0xA5, 0x60])
+STOP_SCAN = bytearray([0xA5, 0x65])
+
+# Scan data category constants.
+CLOUD_DATA = 0
+START_DATA = 1
+
+@dataclass
+class LaserScanPoint:
+    angle: float
+    distance: float 
+
+@dataclass
+class ScanHeader:
+    frequency: float
+    packet_type: int
+    quantity: int
+    start_angle: float
+    end_angle: float
+    check_code: int
+
+
+class G2:
+    _serial: serial.Serial
+
+    def __init__(self, port):
+        self._serial = serial.Serial(port, 230400, timeout=2, write_timeout=2)
+
+    def start_scan_loop(self):
+        self._serial.write(START_SCAN)
+        # TODO: Use coroutine or something else?
+
+    def stop_scan(self):
+        self._serial.write(STOP_SCAN)
+
+    def read_data_once(self, after_iteration=0):
+        # TODO: This is required, but lacks of specific reason. refactor this
+        self.start_scan_loop()
+        self._parse_response()
+        
+        current_iteration: int = 0
+        retrieved: List[LaserScanPoint]
+
+        # Wait until it reaches to specified cycle.
+        retrieved = self._parse_one_cycle()
+        while (current_iteration < after_iteration):
+            current_iteration = current_iteration + 1
+            retrieved = self._parse_one_cycle()
+        return retrieved
+
+    def _parse_response(self):
+        start_sign = self._serial.read(2)
+        response = self._serial.read(4)
+        typecode = self._serial.read()
+    
+    def _parse_one_cycle(self) -> List[LaserScanPoint]: 
+        header_count: int = 0
+        scanned_points: List[LaserScanPoint] = []
+        is_first_header: bool = True
+
+        # Loop before the end of a cycle
+        while True:
+            # detect if this is one of the correct fragment of scan header
+            received = self._serial.read()
+            if received == SCAN_HEADER[header_count]:
+                header_count = header_count + 1
+            if header_count < 2:
+                continue
+
+            header = self._parse_scan_header_fields()
+            parsed_points = self._parse_scan_samples(header)
+            scanned_points.extend(parsed_points)
+
+            if not is_first_header and header.packet_type == START_DATA:
+                break
+
+            is_first_header = False
+            header_count = 0
+        
+        return scanned_points
+
+    def _parse_scan_header_fields(self) -> ScanHeader:
+        # This assumes incoming serial data is aligned correctly.
+        # If failed, this does not yield correct results anymore. 
+        status = self._serial.read()
+        sample_quantity = self._serial.read()
+        fsa_angle = self._serial.read(2)
+        lsa_angle = self._serial.read(2)
+        check_code = self._serial.read(2)
+
+        status = int.from_bytes(status, 'little')
+        frequency = (status >> 1) / 10
+        packet_type = status & 0b1
+        quantity = int.from_bytes(sample_quantity, 'little')
+        fsa = int.from_bytes(fsa_angle, 'little')
+        lsa = int.from_bytes(lsa_angle, 'little')
+
+        check_code = int.from_bytes(check_code, 'little')
+
+        starting_angle = (fsa >> 1) / 64
+        ending_angle = (lsa >> 1) / 64
+        angle_diff = (ending_angle + 360) - starting_angle if ending_angle - starting_angle < 0 else ending_angle - starting_angle
+
+        header_field = ScanHeader(frequency, packet_type, quantity, starting_angle, ending_angle, check_code)
+        return header_field
+
+    def _parse_scan_samples(self, header: ScanHeader) -> List[LaserScanPoint]:
+        # This assumes incoming serial data is aligned correctly.
+        # If failed, this does not yield correct results anymore. 
+        samples = []
+        for i in range(0, header.quantity):
+            sample_data = self._serial.read(3)
+            sample_data = int.from_bytes(sample_data, 'little')
+
+            second_byte = (sample_data >> 8) & 0b11111111
+            third_byte = (sample_data >> 16) & 0b11111111
+
+            angle_diff = (header.end_angle + 360) - header.start_angle if header.end_angle - header.start_angle < 0 else header.end_angle - header.start_angle
+            distance = ((third_byte << 6) + (second_byte >> 2))
+
+            angle = angle_diff / (header.quantity + 1) * (i + 1) + header.start_angle
+            correcting_angle = 0 if distance == 0 else math.atan2(21.8 * (155.3 - distance), (155.3 * distance))
+            final_angle = math.fmod(angle + correcting_angle, 360)
+            final_radian = math.radians(final_angle)
+
+            samples.append(LaserScanPoint(final_angle, distance))
+        return samples
--- main.py
+++ main.py
@@ -1,137 +1,10 @@
-import math
-import serial
-import struct
+from lidar import g2
 from typing import List
-from typing import Tuple
+import math
 
 import matplotlib.pyplot as plt
 
-# Minimum distance constant.
-MINIMUM_DISTANCE = 0.160
-
-# YDLidar protocol category constants.
-YDLIDAR_START_SIGN = bytearray([0xA5, 0x5A])
-SCANNING = 0x81
-HEALTH_STATUS = 0x06
-DEVICE_INQUIRY = 0x04
-
-# Byte constants.
-START_SCAN = 0x60
-STOP_SCAN = 0x65
-GET_DEVICE_INFO = 0x90
-GET_HEALTH_STATUS = 0x92
-INCREASE_SCAN_FREQ = 0x09
-DECREASE_SCAN_FREQ = 0x0A
-INCREASE_SCAN_FREQ_10X = 0x0B
-DECREASE_SCAN_FREQ_10X = 0x0C
-GET_CURRENT_SCAN_FREQ = 0x0D
-GET_CURRENT_RANGE_FREQ = 0xD1
-USE_HEARTBEAT_PROTOCOL = 0xD9
-SOFT_RESTART = 0x40
-
-# Response mode constants.
-CONTINUOUS = 0x1
-SINGLE = 0x0
-
-# Scan data category constants.
-CLOUD_DATA = 0
-START_DATA = 1
-
-# Byte sequence constants.
-SCAN_HEADER = (bytes([0xAA]), bytes([0x55]))
-
-port = input("Enter the port: ")
-cmd_query = input("Enter the command: ")
-
-ser = serial.Serial(port, 230400, timeout=2, write_timeout=2)
-command = bytearray([0xA5, 0x00]) 
-
-if cmd_query == "start":
-    command[1] = START_SCAN 
-elif cmd_query == "stop":
-    command[1] = STOP_SCAN
-elif cmd_query == "dinfo":
-    command[1] = GET_DEVICE_INFO
-elif cmd_query == "dhealth":
-    command[1] = GET_HEALTH_STATUS 
-elif cmd_query == "scanfreq":
-    command[1] = GET_CURRENT_SCAN_FREQ
-elif cmd_query == "rangfreq":
-    command[1] = GET_CURRENT_RANGE_FREQ
-elif cmd_query == "pprotect":
-    command[1] = USE_HEARTBEAT_PROTOCOL
-elif cmd_query == "reset":
-    command[1] = SOFT_RESTART
-else:
-    print("Invalid command given. exiting")
-    exit()
-
-ser.write(command)
-
-start_sign = ser.read(2)
-response = ser.read(4)
-typecode = ser.read()
-
-res_mode = response[3] >> 6
-
-def read_lidar_data_once(after_iteration=0) -> List[Tuple[float, float]]:
-    current_iteration: int = 0
-    header_count: int = 0
-    scanned_points: List[Tuple[float, float]] = []
-
-    while True:
-        data = ser.read()
-        if data == SCAN_HEADER[header_count]:
-            header_count = header_count + 1
-        if header_count < 2:
-            continue
-
-        status = ser.read()
-        sample_quantity = ser.read()
-        fsa_angle = ser.read(2)
-        lsa_angle = ser.read(2)
-
-        status = int.from_bytes(status, 'little')
-        packet_type = status & 0b1
-        quantity = int.from_bytes(sample_quantity, 'little')
-        fsa = int.from_bytes(fsa_angle, 'little')
-        lsa = int.from_bytes(lsa_angle, 'little')
-
-        starting_angle = (fsa >> 1) / 64
-        ending_angle = (lsa >> 1) / 64
-        angle_diff = (ending_angle + 360) - starting_angle if ending_angle - starting_angle < 0 else ending_angle - starting_angle
-
-        should_retrieve_data: bool = current_iteration > after_iteration
-
-        for i in range(0, quantity):
-            sample_data = ser.read(3)
-            sample_data = int.from_bytes(sample_data, 'little')
-
-            second_byte = (sample_data >> 8) & 0b11111111
-            third_byte = (sample_data >> 16) & 0b11111111
-
-            distance = ((third_byte << 6) + (second_byte >> 2))
-
-            angle = angle_diff / (quantity + 1) * (i + 1) + starting_angle
-            correcting_angle = 0 if distance == 0 else math.atan2(21.8 * (155.3 - distance), (155.3 * distance))
-            final_angle = math.fmod(angle + correcting_angle, 360)
-            final_radian = math.radians(final_angle)
-
-            if should_retrieve_data:
-                scanned_points.append((distance, final_radian))
-
-        if should_retrieve_data and packet_type == START_DATA:
-            return scanned_points
-
-        if packet_type == START_DATA:
-            current_iteration = current_iteration + 1
-        header_count = 0
-
-def stop_lidar():
-    command = bytearray([0xA5, STOP_SCAN])
-    ser.write(command)
-
-def visualize(scanned_data: List[Tuple[float, float]]):
+def visualize(scanned_data: List[g2.LaserScanPoint]):
     fig = plt.figure()
     ax = fig.add_subplot(projection='polar')
 
@@ -139,31 +12,14 @@ def visualize(scanned_data: List[Tuple[float, float]]):
     radians = []
 
     for data in scanned_data:
-        dist.append(data[0])
-        radians.append(data[1])
+        dist.append(data.distance)
+        radians.append(math.radians(data.angle))
 
     ax.scatter(radians, dist, s=0.1)
     plt.show()
 
-if start_sign == YDLIDAR_START_SIGN:
-
-    print("Retrieved response successfully.")
-    res_length = struct.unpack('<4B', response)[0]
-    res_type = struct.unpack('<B', typecode)[0]
-
-    print("Response length : ", res_length)
-    print("Response type : ", res_type)
-    print("Response mode : ", res_mode)
-
-    if res_mode == CONTINUOUS:
-        scanned_data = read_lidar_data_once()
-        print(scanned_data)
-        stop_lidar()
-        visualize(scanned_data)
-    elif res_mode == SINGLE:
-        payload = ser.read(res_length)
-        print("Payload: ", payload)
-    else:
-        print("Unknown response mode. Prehaps the packet is corrupted?")
-else:
-    print("Failed to retrieve the response.")
+port = input("Enter port: ")
+g2_lidar = g2.G2(port)
+received = g2_lidar.read_data_once(10)
+visualize(received)
+g2_lidar.stop_scan()
