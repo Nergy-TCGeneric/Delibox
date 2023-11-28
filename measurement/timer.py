a54e2c8e86c9138d35b1eb00f9d1db2d2ee23760
Nergy-TCGeneric <5158870+Nergy-TCGeneric@users.noreply.github.com>
2023-11-29 00:20:56 +0900

    fix: Make timer work
    DESCRIPTION
    
    * The decorator was coded wrong, as it does not return inner funtion.
      fixed.

--- measurement/timer.py
+++ measurement/timer.py
@@ -1,8 +1,10 @@
 import time
 
-def measure_time_in_ns(target):
-    start_time = time.perf_counter_ns()
-    def wrapper_func():
-        return target
-    end_time = time.perf_counter_ns()
-    print(f"Took {end_time - start_time} ns to finish")
+def measure_time_in_ns(target) -> callable:
+    def wrapper_func(*args, **kwargs):
+        start_time = time.perf_counter_ns()
+        result = target(*args, **kwargs)
+        end_time = time.perf_counter_ns()
+        print(f"{target.__name__} : Took {end_time - start_time} ns to finish")
+        return result
+    return wrapper_func
