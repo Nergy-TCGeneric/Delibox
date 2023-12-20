066642e6271ea43ea90218076ad68c5d67affcfd
Nergy-TCGeneric <5158870+Nergy-TCGeneric@users.noreply.github.com>
2023-12-21 07:24:10 +0900

    wip: Oversample the dots to 'stablize' them

--- main.py
+++ main.py
@@ -27,8 +27,46 @@ def visualize_scan_points(scanned_data: List[g2.LaserScanPoint]):
 
 def update_visualizing_image(index):
     scanned_data = g2_lidar.read_data()
-    submap = submapper.lidar_to_submap(scanned_data)
-    global_mapper.update(submap)
+    initial = submapper.lidar_to_submap(scanned_data)
+    iteration_count = 0
+
+    while iteration_count < 9:
+        scanned_data = g2_lidar.read_data()
+        another_submap = submapper.lidar_to_submap(scanned_data)
+        if len(initial.content) < len(another_submap.content):
+            last_point: int = initial.content[-1][-1]
+            print(last_point)
+            shape_1 = another_submap.content.shape
+            shape_2 = initial.content.shape
+            diff_x = shape_1[0] - shape_2[0]
+            diff_y = shape_1[1] - shape_2[1]
+
+            hori_pads = np.ones((diff_x, shape_2[1])) * last_point
+            vert_pads = np.ones((shape_2[0], diff_y))
+            padded = np.concatenate((initial.content, hori_pads), axis=0)
+            padded = np.concatenate((padded, vert_pads), axis=1)
+
+            new_content = padded + another_submap.content
+            initial.content = new_content
+        else:
+            last_point: int = initial.content[-1][-1]
+
+            shape_1 = initial.content.shape
+            shape_2 = another_submap.content.shape
+            diff_x = shape_1[0] - shape_2[0]
+            diff_y = shape_1[1] - shape_2[1]
+
+            hori_pads = np.ones((diff_x, shape_1[1])) * last_point
+            vert_pads = np.ones((shape_1[0], diff_y))
+            padded = np.concatenate((initial.content, hori_pads), axis=0)
+            padded = np.concatenate((padded, vert_pads), axis=1)
+
+            new_content = padded + initial.content
+            initial.content = new_content
+
+        iteration_count = iteration_count + 1
+
+    global_mapper.update(initial)
     imshow.set_data(global_mapper._occupancy_grid.content)
 
 
@@ -68,7 +106,7 @@ initial_read = g2_lidar.read_data()
 initial_grid = submapper.lidar_to_submap(initial_read)
 global_mapper.update(initial_grid)
 
-anim = FuncAnimation(vis, update_visualizing_image, frames=10, interval=16)
+anim = FuncAnimation(vis, update_visualizing_image, frames=100, interval=16)
 
 imshow = ax.imshow(
     global_mapper._occupancy_grid.content, cmap="binary", vmin=0, vmax=255
